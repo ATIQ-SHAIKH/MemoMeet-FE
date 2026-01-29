5c0fe87b8b6fdb54fb4f180839a0e8602659e369
Atiq Shaikh <atikshaikh436@gmail.com>
2026-01-30 01:13:01 +0530

    Added Eslint and Prettier, also added settings for vs code

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    env: {
-        BACKEND_URL: "https://memomeet-be.onrender.com/api",
-        WEBSOCKET_URL: "https://memomeet-be.onrender.com",
-    }
+  reactStrictMode: false,
+  env: {
+    BACKEND_URL: 'https://memomeet-be.onrender.com/api',
+    WEBSOCKET_URL: 'https://memomeet-be.onrender.com',
+  },
 };
 
 export default nextConfig;
