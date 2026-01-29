a7c0bdf32db00724df0c7994dddcc7a3c9666564
Atiq Shaikh <atikshaikh436@gmail.com>
2026-01-30 01:14:13 +0530

    Home page image will be hidden for screen size less than 900

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,30 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { checkSession, createMeetCode } from "@/utils/api";
-import Loader from "@/components/Loader";
-import LandingPageNavBar from "@/components/LandingPageNavBar";
-import LandingPageContent from "@/components/LandingPageContent";
+import { checkSession, createMeetCode } from '@/utils/api';
+import Loader from '@/components/Loader';
+import LandingPageNavBar from '@/components/LandingPageNavBar';
+import LandingPageContent from '@/components/LandingPageContent';
 import LandingPageImage from "@/components/LandingPageImage";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
-  const [time, setTime] = useState("");
-  const [date, setDate] = useState("");
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
-  const [meetCode, setMeetCode] = useState(""); // State to hold meeting code
+  const [meetCode, setMeetCode] = useState(''); // State to hold meeting code
 
   useEffect(() => {
     const updateTime = () => {
       const now = new Date();
-      const formattedTime = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+      const formattedTime = now.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
       setTime(formattedTime);
 
-      const options = { weekday: "short", day: "2-digit", month: "short" };
+      const options = { weekday: 'short', day: '2-digit', month: 'short' };
       const formattedDate = now.toLocaleDateString([], options);
       setDate(formattedDate);
     };
@@ -32,7 +35,7 @@ export default function Home() {
     const checkUserSession = async () => {
       const session = await checkSession();
       setLoggedIn(session); // Set the loggedIn state based on the result of checkSession
-      setTimeout(() => setLoading(false), 100)
+      setTimeout(() => setLoading(false), 100);
     };
 
     checkUserSession();
@@ -43,31 +46,40 @@ export default function Home() {
 
   const handleStartMeetingClick = () => {
     createMeetCode().then((meetCode) => {
-      setLoading(true)
+      setLoading(true);
       router.push(`/meet/${meetCode}`); // Navigate to the meeting room with the meeting code
     });
-  }
+  };
 
   const handleSignInClick = () => {
-    setLoading(true)
+    setLoading(true);
     router.push('/signin');
   };
 
   const handleJoinClick = () => {
-    setLoading(true)
+    setLoading(true);
     router.push(`/meet/${meetCode}`); // Navigate to the meeting room with the meeting code
   };
 
   return (
     <div className="font-[family-name:var(--font-geist-sans)] min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-col">
-      {loading ? (<Loader />) : (
+      {loading ? (
+        <Loader />
+      ) : (
         <>
           <LandingPageNavBar time={time} date={date} />
-          <main className="flex flex-col sm:flex-row items-center justify-center flex-grow">
+          <main className="flex flex-col min-[900px]:flex-row items-center justify-center flex-grow">
             {/* Image Section */}
             <LandingPageImage />
             {/* Text and Buttons Section */}
-            <LandingPageContent handleStartMeetingClick={handleStartMeetingClick} setMeetCode={setMeetCode} handleJoinClick={handleJoinClick} handleSignInClick={handleSignInClick} loggedIn={loggedIn} loading={loading}/>
+            <LandingPageContent
+              handleStartMeetingClick={handleStartMeetingClick}
+              setMeetCode={setMeetCode}
+              handleJoinClick={handleJoinClick}
+              handleSignInClick={handleSignInClick}
+              loggedIn={loggedIn}
+              loading={loading}
+            />
           </main>
         </>
       )}
--- src/components/LandingPageImage.js
+++ src/components/LandingPageImage.js
@@ -1,16 +1,15 @@
-import Image from "next/image";
-
+import Image from 'next/image';
 
 export default function LandingPageImage() {
-    return (
-        <div className="w-full sm:w-1/3 flex justify-center">
-            <Image
-                src="/landing_page_image.svg"
-                alt="An image of two people in a meeting"
-                width={500}
-                height={400}
-                className="object-contain"
-            />
-        </div>
-    );
+  return (
+    <div className="w-full sm:w-1/3 flex justify-center">
+      <Image
+        src="/landing_page_image.svg"
+        alt="An image of two people in a meeting"
+        width={500}
+        height={400}
+        className="object-contain max-[900px]:hidden"
+      />
+    </div>
+  );
 }
