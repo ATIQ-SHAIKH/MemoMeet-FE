35af2446646e21e5263041f53ccbb4bc666acf1c
Atiq Shaikh <atikshaikh436@gmail.com>
2026-02-08 18:24:43 +0530

    Removed env variables from next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  env: {
-    BACKEND_URL: 'https://memomeet-be.onrender.com/api',
-    WEBSOCKET_URL: 'https://memomeet-be.onrender.com',
-  },
 };
 
 export default nextConfig;
